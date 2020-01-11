2b234a328cb1fce30cf45167dfa9499c9d457a87
jacki0 <jackio@tuta.io>
2020-01-11 21:53:11 +0300

    начал добавлять функционал для повторной отправки сообщений

--- heisebot.py
+++ heisebot.py
@@ -1,16 +1,18 @@
 import telebot
-import random
-import string
-import sched
-import time
-bot = telebot.TeleBot('         ')
+from twisted.internet import task, reactor
+
+bot = telebot.TeleBot('887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Бот отправляет сообщение каждые 3 часа',
-    reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Бот отправляет сообщение каждые 3 часа')
 
-@bot.message_handler(content_types=['text'])
-def passwordgeneration(message):
+    timeout = 25.0
+    
+    def msg():
+        bot.send_message(message.chat.id, 'Бот отправляет сообщение каждые 3 часа')
+    
+    loop = task.LoopingCall(msg())
+    loop.start(timeout)
 
 bot.polling()
