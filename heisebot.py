8e9278e58e1da82cf844e64b31398fd3c2805761
jacki0 <jackio@tuta.io>
2020-01-12 20:11:53 +0300

    Изменил функционал отправки сообщения

--- heisebot.py
+++ heisebot.py
@@ -1,18 +1,12 @@
-import telebot
 from twisted.internet import task, reactor
+import requests
 
-bot = telebot.TeleBot('887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo')
+timeout = 25.0
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Бот отправляет сообщение каждые 3 часа')
+def msg():
+    aa = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-453849336text='
+    bb = 'Cообщение'
+    requests.get(aa + bb)
 
-    timeout = 25.0
-    
-    def msg():
-        bot.send_message(message.chat.id, 'Бот отправляет сообщение каждые 3 часа')
-    
-    loop = task.LoopingCall(msg())
-    loop.start(timeout)
-
-bot.polling()
+loop = task.LoopingCall(msg())
+loop.start(timeout)
