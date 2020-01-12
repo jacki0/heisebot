bf3e796dc9149ffd6a33ffeef15c79a0ad421ed6
jacki0 <jackio@tuta.io>
2020-01-12 20:39:01 +0300

    Доработал повторную отправку, пока не работает

--- heisebot.py
+++ heisebot.py
@@ -3,10 +3,11 @@ import requests
 
 timeout = 25.0
 
-def msg():
-    aa = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-453849336text='
-    bb = 'Cообщение'
-    requests.get(aa + bb)
+def msg(message):
+    link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-321091116text='
+    requests.get(link + message)
 
-loop = task.LoopingCall(msg())
+loop = task.LoopingCall(msg('Сообщение'))
 loop.start(timeout)
+
+reactor.run()
