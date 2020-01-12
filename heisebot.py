674ae005a6edef6cb1415a2e98a27343a4ea9f7b
jacki0 <jackio@tuta.io>
2020-01-12 21:26:12 +0300

    Окончательно доработал повторную отправку

--- heisebot.py
+++ heisebot.py
@@ -1,13 +1,11 @@
-from twisted.internet import task, reactor
+import threading
 import requests
 
-timeout = 25.0
 
-def msg(message):
-    link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-321091116text='
+def msg():
+    threading.Timer(5.0, msg).start()
+    link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-321091116&text='
+    message = 'Cпокойной ночи @atlasovNV!'
     requests.get(link + message)
 
-loop = task.LoopingCall(msg('Сообщение'))
-loop.start(timeout)
-
-reactor.run()
+msg()
