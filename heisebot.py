136e70c65820fa5d5ce12a2422b49e2a9042c66f
jacki0 <jackio@tuta.io>
2020-01-13 22:06:16 +0300

    Обновил функцию

--- heisebot.py
+++ heisebot.py
@@ -1,11 +1,10 @@
 import threading
 import requests
 
-
 def msg():
-    threading.Timer(5.0, msg).start()
+    message = 'Привет, @atlasovNV!'
+    threading.Timer(15.0, msg).start()
     link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-321091116&text='
-    message = 'Cпокойной ночи @atlasovNV!'
     requests.get(link + message)
 
 msg()
