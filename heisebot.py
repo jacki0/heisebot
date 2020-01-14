525fb92b13b6308763cf1dcb22900655d2671b94
jacki0 <jackio@tuta.io>
2020-01-14 22:50:42 +0300

    Добавил получение json с сообщениями полученными ботом

--- heisebot.py
+++ heisebot.py
@@ -1,12 +1,20 @@
 import threading
 import requests
+import json
 
 message = 'А я сказал, @atlasovNV будет здесь!!!'
 chat = '-321091116'
+bot = '887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo'
 
-def msg(message, chat):
+def sendmessage(message, chat):
     threading.Timer(5.0, msg, args=(message, chat)).start()
-    link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=' + chat + '&text=' + message
+    link = 'https://api.telegram.org/bot' + bot + '/sendMessage?chat_id=' + chat + '&text=' + message
     requests.get(link)
 
-msg(message, chat)
+def receivingmessage():
+    messages = []
+    req = requests.get('https://api.telegram.org/bot' + bot + '/getupdates')
+    data = json.dumps(req.json(), ensure_ascii=False).encode('utf8').decode().lower().split('"text": ')
+
+#sendmessage(message, chat)
+receivingmessage()
