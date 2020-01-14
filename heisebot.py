63760f000f1d452fd6dd38ffb0fe89b5556c6765
jacki0 <jackio@tuta.io>
2020-01-14 21:19:35 +0300

    Добавил возмоэность задавать сообщение и id чата извне функции

--- heisebot.py
+++ heisebot.py
@@ -1,10 +1,12 @@
 import threading
 import requests
 
-def msg():
-    message = 'Привет, @atlasovNV!'
-    threading.Timer(15.0, msg).start()
-    link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=-321091116&text='
-    requests.get(link + message)
+message = 'А я сказал, @atlasovNV будет здесь!!!'
+chat = '-321091116'
 
-msg()
+def msg(message, chat):
+    threading.Timer(5.0, msg, args=(message, chat)).start()
+    link = 'https://api.telegram.org/bot887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo/sendMessage?chat_id=' + chat + '&text=' + message
+    requests.get(link)
+
+msg(message, chat)
