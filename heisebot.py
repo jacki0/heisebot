e53d4de001305865617e0c7810a5785f7b3f91e6
jacki0 <jackio@tuta.io>
2020-01-16 22:36:21 +0300

    Добавил цикл в функцию получающую сообщение

--- heisebot.py
+++ heisebot.py
@@ -7,14 +7,15 @@ chat = '-321091116'
 bot = '887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo'
 
 def sendmessage(message, chat):
-    threading.Timer(5.0, msg, args=(message, chat)).start()
+    threading.Timer(10800.0, sendmessage, args=(message, chat)).start()
     link = 'https://api.telegram.org/bot' + bot + '/sendMessage?chat_id=' + chat + '&text=' + message
     requests.get(link)
 
 def receivingmessage():
-    messages = []
+    threading.Timer(3590.0, receivingmessage).start()
     req = requests.get('https://api.telegram.org/bot' + bot + '/getupdates')
-    data = json.dumps(req.json(), ensure_ascii=False).encode('utf8').decode().lower().split('"text": ')
+    data = json.dumps(req.json(), ensure_ascii=False).encode('utf8').decode().lower()
+    return data
 
 #sendmessage(message, chat)
 receivingmessage()
