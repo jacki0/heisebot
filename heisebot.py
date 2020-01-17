9ba29940bf921c3df6579e87312451cff0dd3f8d
jacki0 <jackio@tuta.io>
2020-01-17 19:21:17 +0300

    Изменил структуру функций, добавил комментарии

--- heisebot.py
+++ heisebot.py
@@ -4,31 +4,33 @@ import json
 import re
 import string
 
-message = 'А я сказал, @atlasovNV будет здесь!!!'
+
+message = 'test message'
 chat = '-321091116'
 bot = '887793509:AAF_wasq78q0AUX37GgKaFV5IB_hi5NVnbo'
 
-def sendmessage(message, chat):
-    threading.Timer(10800.0, sendmessage, args=(message, chat)).start()
+def sendmessage():
+    threading.Timer(10800.0, sendmessage).start()      # Запуск функции каждые 3 часа
     link = 'https://api.telegram.org/bot' + bot + '/sendMessage?chat_id=' + chat + '&text=' + message
-    requests.get(link)
+    requests.get(link)                                 # Отправка сообщения
+
 
 def receivingmessage():
-    threading.Timer(3590.0, receivingmessage).start()
+    threading.Timer(3590.0, receivingmessage).start()  # Запуск функции каждые 59 минут 50 секунд
     messages = []
     ids = []
     req = requests.get('https://api.telegram.org/bot' + bot + '/getupdates')
     data = json.dumps(req.json(), ensure_ascii=False).encode('utf8').decode().lower().split('"message"')
     for i in data:
-        if '"type": "private"' in i:
+        if '"type": "private"' in i:                   # Проверка, что сообщение было отправлено боту, а не в групповой чат
             i = i.split(', ')
             for j in i:
-                if 'message_id' in j:
+                if 'message_id' in j:                  # Получаем id сообщения, что-бы проверить, что это сообщение ещё не передано в базу
                     j = re.sub('[{}]'.format(re.escape(string.punctuation)), '', j).split(' ')[-1]
                     ids.append(int(j))
-                elif 'text' in j:
+                elif 'text' in j:                      # Получаем текст сообщения
                     j = j.split(': ')[-1]
                     j = re.sub('[{}]'.format(re.escape(string.punctuation)), '', j)
                     messages.append(j)
-    messages.insert(0, max(ids))
+    messages.insert(0, max(ids))                       # Формируем список, где первый элемент - id последнего сообщения, остальные - текст сообщений
     return messages
